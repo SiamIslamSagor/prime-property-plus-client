a40e36365735eb25b1cd8330dcd048a54847653b
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-25 21:23:46 +0600

    modify slider style and show latest reviews in testimunials section

--- src/components/pageComponents/ReviewSlider/ReviewSlider.jsx
+++ src/components/pageComponents/ReviewSlider/ReviewSlider.jsx
@@ -12,6 +12,7 @@ import "./reviewSlider.css";
 
 // import required modules
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
+import { Fade } from "react-awesome-reveal";
 
 const ReviewSlider = ({ latestReviews }) => {
   const progressCircle = useRef(null);
@@ -21,39 +22,51 @@ const ReviewSlider = ({ latestReviews }) => {
     progressContent.current.textContent = `${Math.ceil(time / 1000)}s`;
   };
   return (
-    <>
+    <div className="">
       <Swiper
         spaceBetween={30}
         centeredSlides={true}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
+        // autoplay={{
+        //   delay: 2500,
+        //   disableOnInteraction: false,
+        // }}
         pagination={{
           clickable: true,
         }}
         navigation={true}
         modules={[Autoplay, Pagination, Navigation]}
-        onAutoplayTimeLeft={onAutoplayTimeLeft}
+        // onAutoplayTimeLeft={onAutoplayTimeLeft}
         className="mySwiper"
       >
         {latestReviews?.map(review => (
           <SwiperSlide key={review._id}>
-            <img
-              className="w-full h-full object-cover"
-              src={review.reviewerImg}
-              alt="slide img"
-            />
-            <div className="absolute text-white h-full w-full bg-[#000000a1] flex">
-              <p className="text-center my-auto mx-auto p-4 sm:p-8 md:p-12 sm:text-2xl lg:text-3xl lg:px-32  xl:px-44 2xl:px-60">
-                {review.reviewedPropertyTitle}
-              </p>
-              <p className="text-center my-auto mx-auto p-4 sm:p-8 md:p-12 sm:text-2xl lg:text-3xl lg:px-32  xl:px-44 2xl:px-60">
-                {review.reviewerName}
-              </p>
-              <p className="text-center my-auto mx-auto p-4 sm:p-8 md:p-12 sm:text-2xl lg:text-3xl lg:px-32  xl:px-44 2xl:px-60">
-                {review.reviewerDescription}
-              </p>
+            <div className="w-full flex items-center justify-center flex-col py-10">
+              <div className="max-md:mb-4 md:mb-8">
+                <img
+                  className="w-72 h-64 object-cover rounded-full"
+                  src={review.reviewerImg}
+                  alt="slide img"
+                />
+              </div>
+              <div className=" h-full w-full  flex flex-col gap-2 md:gap-5">
+                <Fade direction="up">
+                  <p className="text-center my-auto mx-auto font-bold text-2xl sm:text-3xl md:text-4xl text-f-color">
+                    I am {review.reviewerName}
+                  </p>
+                </Fade>
+                <Fade delay={600}>
+                  <p className="text-center my-auto mx-auto  text-xl text-t-color">
+                    I provided reviews for this property:{" "}
+                    {review.reviewedPropertyTitle}
+                  </p>
+                </Fade>
+
+                <Fade direction="up" delay={800}>
+                  <p className="text-center my-auto mx-auto sm:max-w-3xl lg:max-w-5xl max-md:text-xl md:text-2xl lg:text-3xl text-p-color">
+                    My Review for this property: {review.reviewerDescription}
+                  </p>
+                </Fade>
+              </div>
             </div>
           </SwiperSlide>
         ))}
@@ -65,7 +78,7 @@ const ReviewSlider = ({ latestReviews }) => {
           <span ref={progressContent}></span>
         </div>
       </Swiper>
-    </>
+    </div>
   );
 };
 
