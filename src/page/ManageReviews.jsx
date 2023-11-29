b568a976df3e46849b4bbd9fa2b1e6ce5e731ee3
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-29 21:23:51 +0600

    load all reviews and show all in manage reviews page for admin

--- src/page/ManageReviews.jsx
+++ src/page/ManageReviews.jsx
@@ -1,7 +1,75 @@
+import { useEffect, useState } from "react";
+import DeleteBtn from "../components/utilitiesComponents/DeleteBtn";
+import SectionTitle from "../components/utilitiesComponents/SectionTitle/SectionTitle";
+import useReviews from "../hooks/useReviews";
+
 const ManageReviews = () => {
+  // hooks
+  const { reviewsData } = useReviews();
+
+  // state
+  const [latestReviews, setLatestReviews] = useState([]);
+
+  //   side effect
+  useEffect(() => {
+    if (reviewsData.length > 0) {
+      const latestReviewsData = reviewsData.sort(
+        (a, b) => new Date(b.reviewTime) - new Date(a.reviewTime)
+      );
+
+      setLatestReviews(latestReviewsData);
+    }
+  }, [reviewsData]);
+
   return (
     <div>
-      <h1 className="text-4xl text-center font-bold">ManageReviews</h1>
+      <div className="my-10 lg:my-20">
+        <SectionTitle heading={"manage reviews"}></SectionTitle>
+      </div>
+      <div>
+        {reviewsData.length === 0 && (
+          <div className="mt-40 text-center">
+            <p className="text-2xl">No review for any Property</p> <br />
+          </div>
+        )}
+
+        <div className="text-white grid justify-items-center grid-cols-1 lg:grid-cols-2 gap-6 container mx-auto">
+          {latestReviews.length !== 0 &&
+            latestReviews?.map(singleReview => (
+              <div
+                className="lg:w-full lg:h-full lg:6 xl:p-10"
+                key={singleReview?._id}
+              >
+                <div className="h-full w-full">
+                  <div className="h-full w-full">
+                    <div className="p-10 mb-5 text-center max-w-lg lg:max-w-xl 2xl:max-w-2xl border-8 glass group bg-p-color w-full h-full">
+                      <div className="max-md:mb-4 md:mb-8 text-center flex justify-center">
+                        <img
+                          className="w-72 h-64 object-cover rounded-full"
+                          src={singleReview.reviewerImg}
+                          alt="slide img"
+                        />
+                      </div>
+                      <h4 className="text-2xl">
+                        Name: {singleReview?.reviewerName}
+                      </h4>
+                      <p className=" text-lg">
+                        Email: {singleReview?.reviewerEmail}
+                      </p>
+                      <p className=" text-lg">
+                        Review Time: {singleReview?.reviewTime}
+                      </p>
+                      <p className="mt-4 leading-5 mb-4 text-lg">
+                        Review Description: {singleReview?.reviewerDescription}
+                      </p>
+                      <DeleteBtn btnText="Delete"></DeleteBtn>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 };
