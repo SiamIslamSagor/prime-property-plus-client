dcf18c998ad7ab352087507d4716b7dba5fafbfc
SiamIslamSagor <msiamislam12345@gmail.com>
2023-11-30 21:01:41 +0600

    show user role in manage user page for admin

--- src/page/ManageUsers.jsx
+++ src/page/ManageUsers.jsx
@@ -34,39 +34,63 @@ const ManageUsers = () => {
                   <tr className="hover" key={user?._id}>
                     <th>{idx + 1}</th>
                     <td className="text-sm lg:text-base">
-                      <div className="flex items-center gap-3">
+                      <div className="">
                         <div>
                           <div className="font-bold">{user?.name}</div>
                         </div>
                       </div>
                     </td>
                     <td className="text-sm lg:text-base">
-                      <div className="flex items-center gap-3">
+                      <div className="">
                         <div>
                           <div className="font-bold">{user?.email}</div>
                         </div>
                       </div>
                     </td>
                     <td className="text-center">
-                      <button className="btn rounded-full  bg-p-color border-p-color text-white hover:border-p-color hover:text-p-color hover:bg-white duration-[350ms] ease-in-out  btn-outline uppercase group max-sm:btn-sm sm:py-3 w-12 h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16  ">
-                        <span className="text-[20px]  max-sm:text-sm">
-                          <FaUserShield></FaUserShield>
-                        </span>{" "}
-                      </button>
+                      {user.role === "admin" ? (
+                        <div className="">
+                          <div>
+                            <div className="font-bold">Admin</div>
+                          </div>
+                        </div>
+                      ) : (
+                        <button className="btn rounded-full  bg-p-color border-p-color text-white hover:border-p-color hover:text-p-color hover:bg-white duration-[350ms] ease-in-out  btn-outline uppercase group max-sm:btn-sm sm:py-3 w-12 h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16  ">
+                          <span className="text-[20px]  max-sm:text-sm">
+                            <FaUserShield></FaUserShield>
+                          </span>{" "}
+                        </button>
+                      )}
                     </td>
                     <td className="text-center">
-                      <button className="btn rounded-full  border-t-color text-t-color hover:bg-t-color hover:border-t-color duration-[350ms] ease-in-out  btn-outline uppercase group max-sm:btn-sm sm:py-3 w-12 h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16">
-                        <span className="text-[20px]  max-sm:text-sm">
-                          <FaUserTie></FaUserTie>
-                        </span>{" "}
-                      </button>
+                      {user.role === "agent" ? (
+                        <div className="">
+                          <div>
+                            <div className="font-bold">Agent</div>
+                          </div>
+                        </div>
+                      ) : (
+                        <button className="btn rounded-full  border-t-color text-t-color hover:bg-t-color hover:border-t-color duration-[350ms] ease-in-out  btn-outline uppercase group max-sm:btn-sm sm:py-3 w-12 h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16">
+                          <span className="text-[20px]  max-sm:text-sm">
+                            <FaUserTie></FaUserTie>
+                          </span>{" "}
+                        </button>
+                      )}
                     </td>
                     <td className="text-center">
-                      <button className="btn rounded-full  bg-f-color border-f-color text-white hover:border-f-color hover:text-f-color hover:bg-white duration-[350ms] ease-in-out  btn-outline uppercase group max-sm:btn-sm sm:py-3 w-12 h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16  ">
-                        <span className="text-[20px]  max-sm:text-sm">
-                          <FaUserAltSlash></FaUserAltSlash>
-                        </span>{" "}
-                      </button>
+                      {user.role === "fraud" ? (
+                        <div className="">
+                          <div>
+                            <div className="font-bold">Fraud</div>
+                          </div>
+                        </div>
+                      ) : (
+                        <button className="btn rounded-full  bg-f-color border-f-color text-white hover:border-f-color hover:text-f-color hover:bg-white duration-[350ms] ease-in-out  btn-outline uppercase group max-sm:btn-sm sm:py-3 w-12 h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16  ">
+                          <span className="text-[20px]  max-sm:text-sm">
+                            <FaUserAltSlash></FaUserAltSlash>
+                          </span>{" "}
+                        </button>
+                      )}
                     </td>
                     <td className="text-center">
                       <button className="btn rounded-full  border-red-600 text-red-600 hover:bg-red-600 hover:border-red-600 duration-[350ms] ease-in-out  btn-outline uppercase group max-sm:btn-sm sm:py-3 w-12 h-12 lg:w-14 lg:h-14 xl:w-16 xl:h-16 ">
